9b0aebb75ef20f2f29c433d64382eff7f4bb392b
agnes-joseph <agnesj0315@gmail.com>
2016-01-27 22:28:42 +0530

    Update toBase.c

--- toBase.c
+++ toBase.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 
-void toBase(int n,int b)
+char* toBase(int n,int b)
 {
     int res,i=0;
-    char  stack[10];
+    char  *stack = (char*) malloc(sizeof(char));
 
     while(n>0)
     {
@@ -21,20 +21,38 @@ void toBase(int n,int b)
         n = n/b;
     }
 
-    for(i--;i>=0;i--)
+    stack[i--] = '\0';
+
+    //reversing the stack
+    int j=0;
+    char temp;
+
+    while(j<i)
     {
-        printf("%c",stack[i]);
+        temp = stack[i];
+        stack[i] = stack[j];
+        stack[j] = temp;
+        i--;
+        j++;
     }
+    return stack;
 }
 
 int main()
 {
     int num,base;
+    char *result;
     printf("Enter number: ");
     scanf("%d",&num);
     printf("Enter base: ");
     scanf("%d",&base);
 
-    printf("Result: ");
-    toBase(num,base);
+    if(base>36 || base<2)
+    {
+        printf("The base must be between 2 and 36 \n");
+        return;
+    }
+
+    result = toBase(num,base);
+    printf("Result: %s",result);
 }
