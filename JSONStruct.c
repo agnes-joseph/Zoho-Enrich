9af4859e83f55ab987d1c0ced92af414d0f6e3a2
agnes-joseph <agnesj0315@gmail.com>
2016-03-05 09:33:48 +0530

    Update JSONStruct.c

--- JSONStruct.c
+++ JSONStruct.c
@@ -1,12 +1,12 @@
-struct Person
+struct Contact
 {
   int contact_id;
   char contact_name[10];
   char street[25],area[10],city[10];
-  struct Contact * contact_persons[10];
+  struct Person *contact_persons[10];
 };
 
-struct Contact 
+struct Person 
 {
   int contact_person_id;
   char first_name[10],last_name[10];
